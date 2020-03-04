5db03ad503c6d4f7d1158079c2c19983dac7d6b2
Higor Ramos <higor.rufino-ramos@leaseplan.com>
2020-03-04 15:44:25 +0100

    Templates in S3

--- tailor.js
+++ tailor.js
@@ -1,13 +1,16 @@
+/* eslint-disable no-console */
 'use strict'
 
 const http = require('http')
 const Tailor = require('node-tailor')
+
+const fetchTemplate = require('./lib/fetch-template')
 const tailor = new Tailor({
-  templatesPath: __dirname + '/templates'
+  fetchTemplate: fetchTemplate('', () => 'base-template')
 })
 
 http
-  .createServer((req, res) => {
+  .createServer(async (req, res) => {
     if (req.url === '/favicon.ico') {
       res.writeHead(200, { 'Content-Type': 'image/x-icon' })
       return res.end('')
@@ -15,9 +18,9 @@ http
 
     req.headers['x-request-uri'] = req.url
     req.url = '/index'
-
     tailor.requestHandler(req, res)
+
   })
-  .listen(8080, function() {
+  .listen(8080, function () {
     console.log('Tailor server listening on port 8080')
   })
